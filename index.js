fca3e33e752864266e5ee86240af865a24c555bb
Renato Junior <momentum323@gmail.com>
2020-07-07 16:06:08 -0300

    write file

--- index.js
+++ index.js
@@ -1,42 +1,37 @@
 const fs = require('fs')
-const path = require('path')
 
-let sistema = ['siaif.pbt', 'sicco.pbt']
+let ext = '.pbt'
+let sistemas = ['siaif', 'sicco']
 
-function listarAruivosDiretorio (dir, files) {
-  if (!files) {
-    files = []
+function listarAruivosDiretorio (dir, file) {
+  if (!file) {
+    file = []
   }
 
-  let file = fs.readdirSync(dir)
-  file.map((item, index) => {
-    let stat = fs.statSync(dir + '/' + file[index])
+  let files = fs.readdirSync(dir)
+  files.map((item, index) => {
+    let stat = fs.statSync(dir + '/' + files[index])
 
     if (stat.isDirectory()) {
-      listarAruivosDiretorio(dir + '/' + file[index], files)
-    } else if (dir + '/' + file[index] ===`./Energisa/Sistemas/TECCOM/SIAIF/fontes/${sistema[0]}`) {
-      return files.push(dir + '/' + file[index])
+      listarAruivosDiretorio(dir + '/' + files[index], file)
+    } else if (dir + '/' + files[index] === `${dir}/${sistemas[0]}${ext}`) {
+      return file.push(dir + '/' + files[index])
     }
   })
 
-  return files.toString()
+  return file.toString()
 }
 
-
-function readFile(file) {
+function readFile (file) {
   const target = fs.readFileSync(file)
   return target.toString('utf-8')
 }
 
-
 let file = readFile(listarAruivosDiretorio('./Energisa')).split(' ')
 
+fs.writeFileSync('file.txt', file)
 
-function preperetRow(row) {
-  row.map(item =>{
-    return item.split(';')
-  })
-}
 
-let row = preperetRow(file)
- console.log(file[14].split(';'))
+
+
+console.log(file)
