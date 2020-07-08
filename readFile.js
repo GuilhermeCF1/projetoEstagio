77ac133e5ea2c8cdf2d57be44c5d891272fb2248
Renato Junior <momentum323@gmail.com>
2020-07-08 08:54:05 -0300

    update function

--- readFile.js
+++ readFile.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 
 function readTarget (target) {
   const readFile = fs.readFileSync(target)
-  return readTarget.toString('utf-8')
+  return readFile.toString('utf-8')
 }
 
 module.exports = readTarget
